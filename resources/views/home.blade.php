97bef6e08de7c4d293a585a07c920fd7e1dab513
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2024-12-13 17:14:41 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+@section('title', 'Page Title')
 
-<body>
-    @if (true)
-    <h1>test</h1>
-    @endif
-</body>
-
-</html>
+@section('content')
+    <p>This is my body content.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
-Route::get('/about', function () {
-    return view('welcome');
-});
