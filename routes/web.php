7f57eddb172c6eed153aaae1c6883d443fcf1a0c
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2024-12-15 06:58:21 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/admin', function () {
-    return view('admin.admin');
+    return view('admin.dashboard');
 });
 
 Route::get('/', function () {
